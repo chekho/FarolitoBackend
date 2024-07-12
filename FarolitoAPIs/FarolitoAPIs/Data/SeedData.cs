3e43787b988d80c42e69fc5b094b4bbbdf02caa0
BravoDario <adriandariobravo@gmail.com>
2024-07-11 22:34:29 -0600

    SeedData Usuario ahora sí

--- FarolitoAPIs/FarolitoAPIs/Data/SeedData.cs
+++ FarolitoAPIs/FarolitoAPIs/Data/SeedData.cs
@@ -30,10 +30,306 @@ namespace FarolitoAPIs.Data
             }
 
             // Registros de usuarios
-            /*if (!context.Usuarios.Any())
+            if (!context.Usuarios.Any())
             {
-                context.Usuarios.AddRange();
-            }*/
+                context.Usuarios.AddRange(
+                    new Usuario
+                  {                      
+                      Nombre = "alexa",
+                      Contraseña = "12345678",
+                      Rol = "Administrador",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Alexa", ApellidoP = "Guerrero", ApellidoM = "López", Correo = "alexa@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "ramirez",
+                      Contraseña = "12345678",
+                      Rol = "Administrador",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Jose Angel", ApellidoP = "Ramirez", ApellidoM = "Almeida", Correo = "almeida@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "alvizo",
+                      Contraseña = "12345678",
+                      Rol = "Administrador",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Angel Eduardo", ApellidoP = "Juarez", ApellidoM = "Alvizo", Correo = "angel@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "chekho",
+                      Contraseña = "12345678",
+                      Rol = "Administrador",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Sergio de Jesús", ApellidoP = "Salazar", ApellidoM = "Cabrera", Correo = "chekho@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "dario",
+                      Contraseña = "12345678",
+                      Rol = "Administrador",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Adrián Darío", ApellidoP = "Bravo", ApellidoM = "Luna", Correo = "adriandario@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Pjuancarlos",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Juan Carlos", ApellidoP = "Pérez", ApellidoM = "López", Correo = "Pjuancarlos@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Gmaríafernanda",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "María Fernanda", ApellidoP = "González", ApellidoM = "Martínez", Correo = "Gmaríafernanda@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Vcarlos",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Carlos", ApellidoP = "Vargas", ApellidoM = "Mendoza", Correo = "Vcarlos@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Nluisa",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Luisa", ApellidoP = "Navarro", ApellidoM = "Ortiz", Correo = "Nluisa@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Jmanuel",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Manuel", ApellidoP = "Jiménez", ApellidoM = "Flores", Correo = "Jmanuel@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Rjoséluis",
+                      Contraseña = "12345678A",
+                      Rol = "Logística",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "José Luis", ApellidoP = "Rodríguez", ApellidoM = "Hernández", Correo = "Rjoséluis@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Ranasofía",
+                      Contraseña = "12345678A",
+                      Rol = "Producción",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Ana Sofía", ApellidoP = "Ramírez", ApellidoM = "Torres", Correo = "Ranasofía@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Oluisfernando",
+                      Contraseña = "12345678A",
+                      Rol = "Almacén",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Luis Fernando", ApellidoP = "Ortiz", ApellidoM = "Delgado", Correo = "Oluisfernando@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Adaniela",
+                      Contraseña = "12345678A",
+                      Rol = "Logística",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Daniela", ApellidoP = "Aguilar", ApellidoM = "Morales", Correo = "Adaniela@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Snatalia",
+                      Contraseña = "12345678A",
+                      Rol = "Almacén",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Natalia", ApellidoP = "Soto", ApellidoM = "Ruiz", Correo = "Snatalia@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Mandrés",
+                      Contraseña = "12345678A",
+                      Rol = "Almacén",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Andrés", ApellidoP = "Molina", ApellidoM = "Castro", Correo = "Mandrés@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Npaola",
+                      Contraseña = "12345678A",
+                      Rol = "Producción",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Paola", ApellidoP = "Núñez", ApellidoM = "Gómez", Correo = "Npaola@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Spedro",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Pedro", ApellidoP = "Sánchez", ApellidoM = "Díaz", Correo = "Spedro@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Mgabriela",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Gabriela", ApellidoP = "Moreno", ApellidoM = "García", Correo = "Mgabriela@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Csofía",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Sofía", ApellidoP = "Castillo", ApellidoM = "Romero", Correo = "Csofía@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Jvaleria",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Valeria", ApellidoP = "Jiménez", ApellidoM = "Silva", Correo = "Jvaleria@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Hjavier",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Javier", ApellidoP = "Herrera", ApellidoM = "Cruz", Correo = "Hjavier@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Gmiguelángel",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Miguel Ángel", ApellidoP = "Guzmán", ApellidoM = "Flores", Correo = "Gmiguelángel@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Rsebastián",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Sebastián", ApellidoP = "Ríos", ApellidoM = "Domínguez", Correo = "Rsebastián@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Llorena",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Lorena", ApellidoP = "López", ApellidoM = "Ortiz", Correo = "Llorena@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Halejandro",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Alejandro", ApellidoP = "Hernández", ApellidoM = "Cortés", Correo = "Halejandro@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Fjulia",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Julia", ApellidoP = "Fernández", ApellidoM = "Serrano", Correo = "Fjulia@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Tguadalupe",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Guadalupe", ApellidoP = "Torres", ApellidoM = "Ramos", Correo = "Tguadalupe@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Bmiguel",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Miguel", ApellidoP = "Bautista", ApellidoM = "Navarro", Correo = "Bmiguel@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Dricardo",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Ricardo", ApellidoP = "Díaz", ApellidoM = "Molina", Correo = "Dricardo@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Lfrancisco",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Francisco", ApellidoP = "Luna", ApellidoM = "Cabrera", Correo = "Lfrancisco@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Mjosé",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "José", ApellidoP = "Martínez", ApellidoM = "González", Correo = "Mjosé@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Ejavier",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Javier", ApellidoP = "Escobar", ApellidoM = "Pérez", Correo = "Ejavier@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Ladriana",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Adriana", ApellidoP = "López", ApellidoM = "Hernández", Correo = "Ladriana@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Dfernando",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Fernando", ApellidoP = "Domínguez", ApellidoM = "Sánchez", Correo = "Dfernando@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Mmariana",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Mariana", ApellidoP = "Martínez", ApellidoM = "López", Correo = "Mmariana@mail.com" }
+                  },
+                  new Usuario
+                  {                      
+                      Nombre = "Fgustavo",
+                      Contraseña = "12345678A",
+                      Rol = "Cliente",
+                      Estatus = 1,
+                      DetallesUsuario = new DetallesUsuario {Nombres = "Gustavo", ApellidoP = "Flores", ApellidoM = "Ramos", Correo = "Fgustavo@mail.com" }
+                  });
+            }
 
             // Registros de proveedores c:
             if (!context.Proveedors.Any()){
@@ -169,6 +465,8 @@ namespace FarolitoAPIs.Data
                 );
                 context.SaveChanges();
             }
+
+            // pendiente: seeddata Inventario 
         }
     }    
 }
