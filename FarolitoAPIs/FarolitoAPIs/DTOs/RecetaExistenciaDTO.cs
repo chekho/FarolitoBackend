d5828098b12816e5855eb60dca1677b7d9376868
IDGS-801-19002051 <156255910+IDGS-801-19002051@users.noreply.github.com>
2024-07-28 21:34:10 -0600

    terminado ventas lamparas

---
+++ FarolitoAPIs/FarolitoAPIs/DTOs/RecetaExistenciaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FarolitoAPIs.DTOs
+{
+    public class RecetaExistenciaDTO
+    {
+        public int RecetaId { get; set; }
+        public string Nombre { get; set; }
+        public int TotalCantidad { get; set; }
+        public bool Existe { get; set; }
+    }
+}
--- FarolitoAPIs/FarolitoAPIs/Models/Recetum.cs
+++ FarolitoAPIs/FarolitoAPIs/Models/Recetum.cs
@@ -1,23 +1,23 @@
-﻿using System.Text.Json.Serialization;
+﻿    using System.Text.Json.Serialization;
 
-namespace FarolitoAPIs.Models;
+    namespace FarolitoAPIs.Models;
 
-public partial class Recetum
-{
-    public int Id { get; set; }
+    public partial class Recetum
+    {
+        public int Id { get; set; }
 
-    public string? Nombrelampara { get; set; }
+        public string? Nombrelampara { get; set; }
 
-    public bool? Estatus { get; set; }
-    public string? Imagen { get; set; }
-    [JsonIgnore]
-    public virtual ICollection<Componentesrecetum> Componentesreceta { get; set; } = new List<Componentesrecetum>();
-    [JsonIgnore]
-    public virtual ICollection<DetallePedido> DetallePedidos { get; set; } = new List<DetallePedido>();
-    [JsonIgnore]
-    public virtual ICollection<Inventariolampara> Inventariolamparas { get; set; } = new List<Inventariolampara>();
-    [JsonIgnore]
-    public virtual ICollection<Solicitudproduccion> Solicitudproduccions { get; set; } = new List<Solicitudproduccion>();
-    [JsonIgnore]
-    public virtual ICollection<Carrito> Carritos { get; set; } = new List<Carrito>();
-}
+        public bool? Estatus { get; set; }
+        public string? Imagen { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Componentesrecetum> Componentesreceta { get; set; } = new List<Componentesrecetum>();
+        [JsonIgnore]
+        public virtual ICollection<DetallePedido> DetallePedidos { get; set; } = new List<DetallePedido>();
+        [JsonIgnore]
+        public virtual ICollection<Inventariolampara> Inventariolamparas { get; set; } = new List<Inventariolampara>();
+        [JsonIgnore]
+        public virtual ICollection<Solicitudproduccion> Solicitudproduccions { get; set; } = new List<Solicitudproduccion>();
+        [JsonIgnore]
+        public virtual ICollection<Carrito> Carritos { get; set; } = new List<Carrito>();
+    }
