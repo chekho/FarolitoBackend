e9b687db446348aad83569a07800fc14a2418a2a
IDGS-801-19002051 <156255910+IDGS-801-19002051@users.noreply.github.com>
2024-11-06 12:47:55 -0600

    Migracion de dashboard
    Se ha migrado la logica del dashboard de python a asp .net

--- FarolitoAPIs/FarolitoAPIs/Models/MejorCliente.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FarolitoAPIs.Models
-{
-    public class MejorCliente
-    {
-        public int Id { get; set; }
-        public string Cliente { get; set; }
-        public double TotalGastado { get; set; }
-    }
-}
